73eff93a92ac40bff1bb73c8c8c8197d012fd13f
hu <>
2019-06-03 19:53:23 +0800

    创建工程

---
+++ iOSFlutterDemo/iOSFlutterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSFlutterDemo
+//
+//  Created by hu on 2019/6/3.
+//  Copyright © 2019 hyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSFlutterDemo/iOSFlutterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSFlutterDemo
+//
+//  Created by hu on 2019/6/3.
+//  Copyright © 2019 hyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
